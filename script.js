28af0c96022b58ab310f79cd0dd85dfa934dbcfe
elorobaldo <elo.robaldo1@gmail.com>
2023-08-04 14:17:00 -0400

    agua de coco

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#form')
+const dados = document.querySelector('#id')
+
+const dados = function(x){  
+  if (x == 5){
+    return "aee";
+  }
+  else{
+    return 4 - x;
+  }
+}
